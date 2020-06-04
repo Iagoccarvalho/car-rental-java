43bf3e2a3ded278787096220e41d51d2d05e43fa
iagoccarvalho <iago@hctech.com.br>
2020-06-04 15:28:24 -0300

    Services created

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,41 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrazilTaxService brazilTaxService;
+	
+	public RentalService() {	
+	}
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService brazilTaxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay= pricePerDay;
+		this.brazilTaxService = brazilTaxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1) /100 /60 /60;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = brazilTaxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+	
+	 
+}
